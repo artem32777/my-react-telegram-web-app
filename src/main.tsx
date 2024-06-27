f4aa8f2f72d3fe817897ad7b2b36e7cfc8a6ab1e
Artem Loshinin <109870624+artem32777@users.noreply.github.com>
2024-06-27 05:04:19 +0400

    0.1.23

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import WebApp from '@twa-dev/sdk'
-// import botapi from './botapi.ts'
-// botapi();
 WebApp.ready();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
